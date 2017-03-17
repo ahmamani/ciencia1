5145e89c858e1683ce9a294295ced7b7d64ebe1c
DOCENTE-G <DOCENTE-G@UCSP.AULAB>
2017-03-17 12:36:14 -0500

    adicionando mensajes

--- primerProyecto/main.cpp
+++ primerProyecto/main.cpp
@@ -5,5 +5,9 @@ using namespace std;
 int main()
 {
     cout << "Hello world!" << endl;
+    cout << "Hello world!" << endl;
+    cout << "Hello world!" << endl;
+    cout << "Hello world!" << endl;
+    cout << "Hello world!" << endl;
     return 0;
 }
