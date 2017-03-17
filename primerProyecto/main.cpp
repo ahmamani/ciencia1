0e5ffed9782d05de19b4fa9d378c5c6a7d87e4be
ahmamani <ahmamani@ucsp.edu.pe>
2017-03-17 12:36:23 -0500

    Update main.cpp

--- primerProyecto/main.cpp
+++ primerProyecto/main.cpp
@@ -9,5 +9,6 @@ int main()
     cout << "Hello world!" << endl;
     cout << "Hello world!" << endl;
     cout << "Hello world!" << endl;
+    cout << "Hello world!" << endl;cout << "Hello world!" << endl;cout << "Hello world!" << endl;
     return 0;
 }
